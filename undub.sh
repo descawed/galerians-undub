a567651d018e096b81e6e4f88244994a8a328511
descawed <github@descawed.com>
2023-11-29 09:20:57 -0500

    Complete FMV patching

--- undub.sh
+++ undub.sh
@@ -41,6 +41,14 @@ mxa() {
     python3 -m galsdk.xa mxa "$@"
 }
 
+header() {
+    echo
+    echo '########################################'
+    printf '# %-36s #\n' "$*"
+    echo '########################################'
+    echo
+}
+
 export_videos() {
     jpsxdec -x ./discs/jp_disc$1.bin.idx -a video -dir ./movies/videos -quality high -vf avi:rgb -up Lanczos3
     # move all videos to the root videos directory
@@ -104,7 +112,7 @@ do
 done
 
 # copy disc images
-echo Copying disc images
+header Copying disc images
 if [[ "$force_jp_copy" = true || ! -e ./discs/jp_disc1.bin ]]
 then
     cp -f "$jp_disc1_in" ./discs/jp_disc1.bin
@@ -133,7 +141,7 @@ then
 fi
 
 # create indexes
-echo Creating indexes
+header Creating indexes
 for f in ./discs/*.bin
 do
     index_path=./discs/$(basename "$f").idx
@@ -146,7 +154,7 @@ done
 # export videos
 # jPSXdec doesn't have an option to skip files that already exist, so rather than exhaustively list all files, we'll
 # just check for the last one on each disc. if that was exported, that should mean everything else was as well.
-echo Exporting videos
+header Exporting videos
 if [[ "$force_export" = true || ! -e ./movies/videos/B_M18XA.avi ]]
 then
     export_videos 1
@@ -170,15 +178,16 @@ rmdir_safe ./movies/videos/T4
 # apply subtitles
 if [[ "$skip_subs" = false ]]
 then
-    echo Applying subtitles
+    header Applying subtitles
 
+    rm -f ./movies/raw/*.STR
     for f in ./movies/subs/*
     do
         rm -f ./movies/frames/*.png
         video_name=$(basename "$f" | cut -d'-' -f1)
         file_index=$(basename "$f" | cut -d'-' -f2 | cut -d'.' -f1)
         patch_path=./movies/patches/$video_name.xml
-        ffmpeg -i ./movies/videos/$video_name.avi -vf subtitles="$f" -vsync 0 ./movies/frames/%04d.png
+        ffmpeg -i ./movies/videos/$video_name.avi -vf subtitles="$f" -fps_mode passthrough ./movies/frames/%d.png
         if [[ "$force_patch" = true || ! -e "$patch_path" ]]
         then
             echo \<?xml version=\"1.0\"?\> > $patch_path
@@ -206,11 +215,116 @@ then
                 disc_num=1
                 ;;
         esac
+
+        # for the stage A and stage D intro videos, we need to patch in the English title text
+        if [[ "$video_name" = M02XA || "$video_name" = D_M01XA ]]
+        then
+            rm -f ./movies/title_frames/*.png
+
+            # it's important that the x and y coordinates be even because ffmpeg's overlay filter seems to have a limitation
+            # where it can only overlay at even coordinates, so odd coordinates will result in the overlaid image being offset
+            # by 1 pixel.
+            if [[ "$video_name" = M02XA ]]
+            then
+                jp_frame=449
+                na_frame=443
+                na_index=1911
+                num_frames=112
+                x=34
+                y=68
+                w=250
+                h=39
+            else
+                jp_frame=548
+                na_frame=546
+                na_index=1911
+                num_frames=111
+                x=90
+                y=68
+                w=139
+                h=38
+            fi
+
+            # extract frames from the English version
+            jpsxdec -x ./discs/na_disc$disc_num.bin.idx -i "$na_index" -dir ./movies/title_frames -quality high -vf png -up Lanczos3
+            # move all frames to the root directory
+            for f in $(find ./movies/title_frames/T4 -type f -name \*.png)
+            do
+                name=$(basename $f | cut -d'[' -f3 | cut -d']' -f1 | bc)
+                mv "$f" ./movies/title_frames/$name.png
+            done
+
+            # patch English title text into Japanese frame
+            while (( "$num_frames" ))
+            do
+                # pre-increment jp_frame because ffmpeg uses 1-based indexes
+                jp_path=./movies/frames/$(( ++jp_frame )).png
+                # ffmpeg can't edit files in place, so output to a temp image, remove the original, then rename the temp
+                ffmpeg -y -i "$jp_path" -i ./movies/title_frames/$(( na_frame++ )).png -filter_complex "[1]crop=$w:$h:$x:$y[t];[0][t]overlay=$x:$y" -update 1 ./movies/frames/tmp.png
+                rm -f "$jp_path"
+                mv ./movies/frames/tmp.png "$jp_path"
+                ((num_frames--))
+            done
+        fi
+
         jpsxdec -x ./discs/jp_disc$disc_num.bin.idx -i "$file_index" -replaceframes "$patch_path"
-        # TODO: extract raw file and patch into NA disc here
+        # now that the video stream has been patched, extract the raw file
+        jpsxdec -x ./discs/jp_disc$disc_num.bin.idx -i $(( file_index - 1 )) -dir ./movies/raw -raw
     done
 
+    # patch raw videos into the NA discs
+    header Patching NA FMVs
+
+    for f in $(find ./movies/raw/T4 -type f -name \*.STR)
+    do
+        name=$(basename $f | cut -d'.' -f1)
+        mv "$f" ./movies/raw/$name.STR
+    done
+
+    for f in ./movies/raw/*.STR
+    do
+        video_name=$(basename "$f")
+        case $(cut -c 1-2 <<< "$video_name") in
+            C_)
+                disc_num=2
+                suffix=_C
+                ;;
+            D_)
+                disc_num=3
+                suffix=_D
+                ;;
+            B_)
+                disc_num=1
+                suffix=_B
+                ;;
+            *)
+                disc_num=1
+                suffix=''
+                ;;
+        esac
+
+        cdpatch -r ./discs/na_disc$disc_num.bin "\T4\MOV$suffix\\$video_name;1" "$f"
+    done
+
+    # cleanup
     rm -f ./movies/frames/*.png
+    rm -f ./movies/title_frames/*.png
+    rm -f ./movies/raw/*.STR
+    rmdir_safe ./movies/frames/T4/MOV
+    rmdir_safe ./movies/frames/T4/MOV_B
+    rmdir_safe ./movies/frames/T4/MOV_C
+    rmdir_safe ./movies/frames/T4/MOV_D
+    rmdir_safe ./movies/frames/T4
+    rmdir_safe ./movies/title_frames/T4/MOV
+    rmdir_safe ./movies/title_frames/T4/MOV_B
+    rmdir_safe ./movies/title_frames/T4/MOV_C
+    rmdir_safe ./movies/title_frames/T4/MOV_D
+    rmdir_safe ./movies/title_frames/T4
+    rmdir_safe ./movies/raw/T4/MOV
+    rmdir_safe ./movies/raw/T4/MOV_B
+    rmdir_safe ./movies/raw/T4/MOV_C
+    rmdir_safe ./movies/raw/T4/MOV_D
+    rmdir_safe ./movies/raw/T4
 fi
 
 echo Done
